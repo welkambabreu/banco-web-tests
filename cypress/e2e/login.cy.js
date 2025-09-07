843f11f38a06ec6c47d95661d7bfc551b5396f00
welkambabreu <welkambabreu@gmail.com>
2025-09-07 17:52:03 -0300

    Adicionando screenshots e videos

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,22 +1,25 @@
 describe('Login', () => {
   beforeEach(() =>{
     cy.visit('http://localhost:4000')
+    cy.screenshot('apos-visitar-pagina')
 
   })
 
-  it('Login com dados válidos deve permitir entrada no sistema ', () => {
+  it.only('Login com dados válidos deve permitir entrada no sistema ', () => {
     
-    cy.get('#username').click().type('julio.lima');   
-    cy.get('#senha').click().type('123456');
+    cy.get('#username').click().type('julio.lima')
+    cy.get('#senha').click().type('123456')
+    cy.screenshot('apos-preencher-dados-validos')
     cy.get('#login-section > .btn').click()
+    cy.screenshot('apos-clicar-no-botao-entrar')
 
     cy.contains('h4', 'Realizar Transferência').should('be.visible')
   })
 
   it('Login com dados inválidos deve apresentar mensagem de erro', () => {
    
-    cy.get('#username').click().type('julio.lima');   
-    cy.get('#senha').click().type('654321');
+    cy.get('#username').click().type('julio.lima')
+    cy.get('#senha').click().type('654321')
     cy.contains('button', 'Entrar').click()
 
     cy.get('.toast').should('have.text', 'Erro no login. Tente novamente.')
